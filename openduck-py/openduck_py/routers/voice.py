c01f2f1ab3b186f60bb59114661baa31ef0d6d32
williamluer <will.luer@gmail.com>
2024-02-24 00:36:30 +0000

    wip

--- openduck-py/openduck_py/routers/voice.py
+++ openduck-py/openduck_py/routers/voice.py
@@ -5,6 +5,7 @@ from tempfile import NamedTemporaryFile
 from time import time
 from typing import Optional
 import wave
+import requests 
 
 # NOTE(zach): On Mac OS, the first import fails, but the subsequent one
 # succeeds. /shrug.
@@ -27,12 +28,23 @@ from openduck_py.db import get_db_async, AsyncSession
 from openduck_py.prompts import prompt
 from openduck_py.voices import styletts2
 from openduck_py.routers.templates import generate
+from openduck_py.utils.speaker_identification import segment_audio, load_pipelines
+
+pipeline, inference = load_pipelines()
+
+with open("aec-cartoon-degraded.wav", "wb") as f:
+    f.write(
+        requests.get(
+            "https://s3.us-west-2.amazonaws.com/quack.uberduck.ai/aec-cartoon-degraded.wav"
+        ).content
+    )
+
+speaker_embedding = inference("aec-cartoon-degraded.wav")
 
 asr_model = asr_models.EncDecCTCModelBPE.from_pretrained(
     model_name="nvidia/stt_en_fastconformer_ctc_large"
 )
 normalizer = Normalizer(input_case="cased", lang="en")
-
 audio_router = APIRouter(prefix="/audio")
 
 
@@ -132,6 +144,21 @@ class ResponseAgent:
             return audio_chunk_bytes
 
         loop = asyncio.get_running_loop()
+
+        rec_time = time()
+        wavfile.write(f"{rec_time}_before.wav", 16000, audio_data)
+        print(f"Audio data shape before: {audio_data.shape}")
+
+        np.save(f"{rec_time}_audio_data.npy", audio_data)
+        audio_data = segment_audio(
+            audio_data=audio_data,
+            sample_rate=16000,
+            speaker_embedding=speaker_embedding,
+            pipeline=pipeline,
+            inference=inference,
+        )
+        print(f"Audio data shape after: {audio_data.shape}")
+        wavfile.write(f"{rec_time}_after.wav", 16000, audio_data)
 
         t0 = time()
 
--- openduck-py/openduck_py/utils/speaker_identification.py
+++ openduck-py/openduck_py/utils/speaker_identification.py
@@ -1,9 +1,13 @@
+import time
+import os
+from typing import Dict
+
 import torch
 import numpy as np
-from pyannote.audio import Pipeline, Inference, Model
 from scipy.spatial.distance import cdist
-from typing import Dict
-import os
+from pyannote.core import Segment
+from pyannote.audio import Pipeline, Inference, Model
+
 
 HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
 
@@ -27,35 +31,107 @@ def load_pipelines() -> tuple:
     return pipeline, inference
 
 
+# def identify_speakers(
+#     audio_data: torch.Tensor,
+#     sample_rate: int,
+#     speaker_embedding: np.array,
+#     pipeline: Pipeline,
+#     inference: Inference,
+# ) -> Dict:
+#     """
+#     Segment the speaker in the input file and return the segments and their distances from the speaker embedding.
+
+#     Args:
+#         input_filename (str): The input file to segment.
+#         speaker_embedding (np.array): The speaker embedding to compare against.
+#         pipeline (Pipeline): The diarization pipeline.
+#         inference (Inference): The embedding inference pipeline.
+
+#     Returns:
+#         Dict: The segments and their distances from the speaker embedding.
+
+#     """
+#     # NOTE(wrl): Diarization takes about 25x longer than inference (0.62 seconds vs 0.024 seconds). Can we only run
+#     # the inference on 1 second chunks?
+#     speaker_segments = {}
+#     start = time.time()
+#     pyannote_input = {"waveform": audio_data, "sample_rate": sample_rate}
+#     output = pipeline(pyannote_input)
+#     print(f"Speaker diarization took {time.time() - start:.3f} seconds")
+#     for segment, _, speaker in output.itertracks(yield_label=True):
+#         start = time.time()
+#         print(f"Shape of audio data: {audio_data.shape}")
+#         print(f"Segment: {segment.start} - {segment.end}")
+#         print(
+#             f"length of audio data: {len(audio_data[1])} and sample rate: {sample_rate} so duration is {len(audio_data) / sample_rate}"
+#         )
+#         segment_embedding = inference.crop(pyannote_input, segment)
+#         print(f"Embedding inference took {time.time() - start:.3f} seconds")
+#         distance = cdist([segment_embedding], [speaker_embedding], metric="cosine")[
+#             0, 0
+#         ]
+#         if speaker not in speaker_segments:
+#             speaker_segments[speaker] = {"times": [], "distances": []}
+#         speaker_segments[speaker]["times"].append((segment.start, segment.end))
+#         speaker_segments[speaker]["distances"].append(distance)
+
+#     return speaker_segments
+
+
 def identify_speakers(
-    input_filename: str,
+    audio_data: torch.Tensor,
+    sample_rate: int,
     speaker_embedding: np.array,
-    pipeline: Pipeline,
-    inference: Inference,
+    pipeline,
+    inference,
 ) -> Dict:
     """
     Segment the speaker in the input file and return the segments and their distances from the speaker embedding.
 
     Args:
-        input_filename (str): The input file to segment.
+        audio_data (torch.Tensor): The waveform of the input audio.
+        sample_rate (int): The sample rate of the input audio.
         speaker_embedding (np.array): The speaker embedding to compare against.
-        pipeline (Pipeline): The diarization pipeline.
-        inference (Inference): The embedding inference pipeline.
+        pipeline: The diarization pipeline.
+        inference: The embedding inference pipeline.
 
     Returns:
         Dict: The segments and their distances from the speaker embedding.
-
     """
     speaker_segments = {}
-    output = pipeline(input_filename)
+    start = time.time()
+    pyannote_input = {"waveform": audio_data, "sample_rate": sample_rate}
+    output = pipeline(pyannote_input)
+    print()
+    print(f"Speaker diarization took {time.time() - start:.3f} seconds")
+
+    audio_length_seconds = (
+        audio_data.shape[1] / sample_rate
+    )  # Corrected to use the second dimension of audio_data
+
     for segment, _, speaker in output.itertracks(yield_label=True):
-        segment_embedding = inference.crop(filename, segment)
+        # Boundary check for the segment
+        segment_end = min(segment.end, audio_length_seconds)
+        if segment.start >= segment_end or segment.end - segment.start < 0.3:
+            continue  # Skip segments that are out of bounds or have no duration
+
+        start = time.time()
+        print(f"Shape of audio data: {audio_data.shape}")
+        print(f"Segment: {segment.start} - {segment_end}")
+        print(
+            f"Length of audio data: {audio_data.shape[1]} samples and sample rate: {sample_rate} so duration is {audio_length_seconds} seconds"
+        )
+        adjusted_segment = Segment(segment.start, segment_end)
+
+        segment_embedding = inference.crop(pyannote_input, adjusted_segment)
+        print(f"Embedding inference took {time.time() - start:.3f} seconds")
+
         distance = cdist([segment_embedding], [speaker_embedding], metric="cosine")[
             0, 0
         ]
         if speaker not in speaker_segments:
             speaker_segments[speaker] = {"times": [], "distances": []}
-        speaker_segments[speaker]["times"].append((segment.start, segment.end))
+        speaker_segments[speaker]["times"].append((segment.start, segment_end))
         speaker_segments[speaker]["distances"].append(distance)
 
     return speaker_segments
@@ -78,17 +154,102 @@ def filter_voices(d: dict, threshold: float = 0.5):
     return new_d
 
 
-if __name__ == "__main__":
-    from pprint import pprint
+import numpy as np
+import torch
+from typing import List
 
-    filename = "../../../samples/speaker-seg-test.wav"
-    pipeline, inference = load_pipelines()
-    speaker_embedding = inference("../../../samples/aec-cartoon.wav")
+
+import numpy as np
+import torch
+
+
+def segment_audio(
+    audio_data: np.array,
+    sample_rate: int,
+    speaker_embedding: np.array,
+    pipeline,
+    inference,
+) -> np.array:
+
+    # converts to [1, n] shape if not already
+    # audio_data = audio_data.reshape(1, -1) if len(audio_data.shape) == 1 else audio_data
+    audio_data_tensor = torch.tensor(audio_data).unsqueeze(0)
+    # Identify speakers
+    start = time.time()
     speaker_segments = identify_speakers(
-        filename, speaker_embedding, pipeline, inference
+        audio_data=audio_data_tensor,
+        sample_rate=sample_rate,
+        speaker_embedding=speaker_embedding,
+        pipeline=pipeline,
+        inference=inference,
     )
+    print(f"Speaker identification took {time.time() - start:.3f} seconds")
 
-    pprint(speaker_segments, width=1)
-
+    # Filter identified speaker segments
+    start = time.time()
     speaker_segments = filter_voices(speaker_segments)
-    pprint(speaker_segments, width=1)
+    print(f"Speaker filtering took {time.time() - start:.3f} seconds")
+    BUFFER = 0.1  # seconds
+    concatenated_audio_data = np.array([], dtype=np.float32)
+
+    start_time = time.time()
+    for speaker, data in speaker_segments.items():
+        for start, end in data["times"]:
+            start = max(0, start - BUFFER)
+            end = min(len(audio_data) / sample_rate, end + BUFFER)
+            segment = audio_data[int(start * sample_rate) : int(end * sample_rate)]
+            print(f"Audio data shape: {audio_data.shape}")
+            print(f"Segment shape: {segment.shape}")
+            print(f"Concatenated audio shape: {concatenated_audio_data.shape}")
+            concatenated_audio_data = np.concatenate((concatenated_audio_data, segment))
+
+    print(f"Audio concatenation took {time.time() - start_time:.3f} seconds")
+    return concatenated_audio_data
+
+
+if __name__ == "__main__":
+    # print(speaker_embedding)
+    # from pprint import pprint
+    # import librosa
+
+    # filename = "../../../samples/speaker-seg-test.wav"
+    pipeline, inference = load_pipelines()
+    # speaker_embedding = inference("../../../samples/aec-cartoon.wav")
+    # # speaker_embedding
+    # librosa_audio, sr = librosa.load(filename, sr=16000)
+    # # # speaker_segments = identify_speakers(
+    # # #     audio_data=librosa_audio,
+    # # #     sample_rate=sr,
+    # # #     speaker_embedding=speaker_embedding,
+    # # #     pipeline=pipeline,
+    # # #     inference=inference,
+    # # # )
+
+    # # # pprint(speaker_segments, width=1)
+
+    # # # speaker_segments = filter_voices(speaker_segments)
+    # # # pprint(speaker_segments, width=1)
+
+    # filtered_audio_data = segment_audio(
+    #     audio_data=librosa_audio,
+    #     sample_rate=sr,
+    #     speaker_embedding=speaker_embedding,
+    #     pipeline=pipeline,
+    #     inference=inference,
+    # )
+    # # # convert to wavfile and write to filtered.wav
+
+    # from scipy.io.wavfile import write
+
+    # write("filtered2.wav", sr, filtered_audio_data)
+
+    #
+    audio_data = np.load('../../1708722359.8986475_audio_data.npy')
+    audio_data = torch.tensor(audio_data).view(1, -1)
+    input = {"waveform": audio_data, "sample_rate": 16000}
+    
+    for i in range(150, 120, -1):
+        end_time = i / 100
+        print(f"Testing end time: {end_time}")
+        inference.crop(input, Segment(1,end_time))
+    # inference.crop(input, Segment(1,1.35))
