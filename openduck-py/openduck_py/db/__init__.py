6e44de4cd6bf07b472ddbb3ce103bcaad79ef4a7
Matthew Kennedy <mtthwknnd4@gmail.com>
2024-02-27 20:13:10 +0000

    fix connection string

--- openduck-py/openduck_py/db/__init__.py
+++ openduck-py/openduck_py/db/__init__.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import configure_mappers, declarative_base, sessionmaker
 DB_USER = "uberduck"
 
 connection_string = "sqlite:////openduck-py/openduck-py/test.db"
-async_connection_string = "sqlite+aiosqlite:///test.db"
+async_connection_string = "sqlite+aiosqlite:////openduck-py/openduck-py/test.db"
 
 
 
