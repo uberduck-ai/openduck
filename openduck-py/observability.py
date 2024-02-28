9185a4de436c2e2a36d5d3fc428ebcad61351526
Matthew Kennedy <mtthwknnd4@gmail.com>
2024-02-28 18:00:48 -0500

    checkboxes (#25)

--- openduck-py/observability.py
+++ openduck-py/observability.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from typing import Dict
 from sqlalchemy.future import select
 from openduck_py.db import connection_string
 from openduck_py.models import DBChatRecord
@@ -16,8 +17,10 @@ def get_chat_records(session_id):
     records = db.execute(stmt).scalars().all()
     return records
 
-def display_chat_interface(records):
+def display_chat_interface(records, show_events: Dict[str, bool]):
     for record in records:
+        if not show_events[record.event_name]:
+            continue
         role = "user"
         if record.event_name in ["generated_completion", "generated_tts", "normalized_text"]:
             role = "assistant"
@@ -37,6 +40,17 @@ stmt = select(DBChatRecord.session_id).distinct().order_by(DBChatRecord.timestam
 unique_session_ids = db.execute(stmt).all()
 session_id_options = [session_id[0] for session_id in unique_session_ids]
 session_id_input = st.selectbox("Select Session ID to display records:", session_id_options)
+
+unique_event_names = db.execute(select(DBChatRecord.event_name).distinct()).all()
+event_name_options = [event_name[0] for event_name in unique_event_names]
+st.sidebar.write("Select events to show:")
+show_events = {}
+for event in event_name_options:
+    value = False
+    if event in ["generated_tts", "generated_completion", "transcribed_audio", "started_response"]:
+        value = True
+    show_events[event] = st.sidebar.checkbox(event, value=value)
+
 if session_id_input:
     records = get_chat_records(session_id_input)
-    display_chat_interface(records)
+    display_chat_interface(records, show_events)
