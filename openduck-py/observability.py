a2da1ed5dab73260a2c176341fcb90868ecc7fb2
Matthew Kennedy <mtthwknnd4@gmail.com>
2024-02-28 21:58:37 +0000

    avatar and markdown header

--- openduck-py/observability.py
+++ openduck-py/observability.py
@@ -22,12 +22,15 @@ def display_chat_interface(records):
         if record.event_name in ["generated_completion", "generated_tts", "normalized_text"]:
             role = "assistant"
         meta_json = record.meta_json or {}
-        with st.chat_message(role):
-            st.text(f"{record.event_name} {record.timestamp}")
+        avatar = None
+        if record.event_name in ["started_session", "detected_start_of_speech", "detected_end_of_speech", "normalized_text", "interrupted_response"]:
+            avatar = "⚙️" 
+        with st.chat_message(role, avatar=avatar):
+            st.write(f"{record.event_name} {record.timestamp}")
             if "audio_url" in meta_json:
                 st.audio(meta_json.get("audio_url"), format="audio/wav", start_time=0)
             elif "text" in meta_json:
-                st.markdown(meta_json.get("text"))
+                st.markdown("##### "+meta_json.get("text"))
 
 
 stmt = select(DBChatRecord.session_id).distinct().order_by(DBChatRecord.timestamp.desc())
