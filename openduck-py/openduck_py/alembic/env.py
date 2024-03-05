bbc3ff2d990a33c6728d9e8102988943a94d1fe8
Matthew Kennedy <mtthwknnd4@gmail.com>
2024-03-05 19:59:54 +0000

    Delete unused tables

--- openduck-py/openduck_py/alembic/env.py
+++ openduck-py/openduck_py/alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from openduck_py.db import connection_string
-from openduck_py.models import DBVoice
+from openduck_py.models import DBChatRecord
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = DBVoice.metadata
+target_metadata = DBChatRecord.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
