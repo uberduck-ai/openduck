77315bfe5b992c3b30d131f5d0190d199050e0ec
hecko <hecko@uberduck.ai>
2024-03-07 00:19:14 +0000

    switch to LiteLLM, make chat and echo detection models settable

--- openduck-py/openduck_py/routers/templates.py
+++ openduck-py/openduck_py/routers/templates.py
@@ -1,43 +1,25 @@
-from typing import Dict, List, Any, Literal
+from typing import Dict, List, Any
 import os
 
-from pydantic import BaseModel
-from openai import AsyncAzureOpenAI
+from litellm import acompletion, ModelResponse
 import jinja2
 
-client = AsyncAzureOpenAI(
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    api_version="2023-05-15",
-)
+from openduck_py.settings import CHAT_MODEL
 
-
-ModelLiteral = Literal["gpt-35-turbo-deployment", "gpt-4-deployment"]
-DEFAULT_MODEL: ModelLiteral = "gpt-35-turbo-deployment"
-
-
-class GenerationResponse(BaseModel):
-    """
-    OpenAI Chat Completions response format:
-    https://platform.openai.com/docs/guides/text-generation/chat-completions-api
-    """
-
-    choices: List
-    created: int
-    id: str
-    model: str
-    object: str
-    usage: Any
+# to not break existing env files
+os.environ["AZURE_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY")
+os.environ["AZURE_API_BASE"] = os.getenv("AZURE_OPENAI_ENDPOINT")
+os.environ["AZURE_API_VERSION"] = "2023-05-15"
 
 
 # TODO (Matthew): Move these to utils, or just call the chat completions API without wrapper functions
 
 
-async def open_ai_chat_continuation(
-    messages: List[Dict[str, str]], model: ModelLiteral
-) -> GenerationResponse:
-    response = await client.chat.completions.create(
-        model=model or DEFAULT_MODEL, messages=messages, temperature=0.3
+async def chat_continuation(
+    messages: List[Dict[str, str]], model: str = CHAT_MODEL
+) -> ModelResponse:
+    response = await acompletion(
+        model=model, messages=messages, temperature=0.3
     )
     return response
 
@@ -45,14 +27,14 @@ async def open_ai_chat_continuation(
 async def generate(
     template: str,
     variables: Dict[str, Any],
-    model: ModelLiteral,
+    model: str = CHAT_MODEL,
     role="user",
-) -> GenerationResponse:
+) -> ModelResponse:
     jinja_template = jinja2.Template(template)
 
     prompt = [{"content": jinja_template.render(variables), "role": role}]
 
-    response = await client.chat.completions.create(
-        model=model or DEFAULT_MODEL, messages=prompt, temperature=0.3
+    response = await acompletion(
+        model=model, messages=prompt, temperature=0.3
     )
     return response
--- openduck-py/openduck_py/settings/__init__.py
+++ openduck-py/openduck_py/settings/__init__.py
@@ -8,3 +8,5 @@ DEPLOY_ENV = os.environ.get("DEPLOY_ENV", "dev")
 IS_DEV = DEPLOY_ENV == "dev"
 CHUNK_SIZE = 1024  # NOTE (Sam): this esp32-specific parameter causes it to fail - 32768 is better on my dev box but still not perfect
 LOG_TO_SLACK = bool(os.environ.get("LOG_TO_SLACK", False))
+CHAT_MODEL = "azure/gpt-35-turbo-deployment"
+UTILITY_MODEL = "azure/gpt-35-turbo-deployment"
