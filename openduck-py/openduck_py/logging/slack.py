b0e49c726a8e780162ba8b111c68d7b96d0393df
Matthew Kennedy <mtthwknnd4@gmail.com>
2024-03-07 14:17:12 +0000

    formatting

--- openduck-py/openduck_py/logging/slack.py
+++ openduck-py/openduck_py/logging/slack.py
@@ -7,11 +7,11 @@ LOGGING_BUCKET = os.environ.get("OUTPUT_BUCKET")
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 SLACK_LOGS_CHANNEL_ID = os.environ.get("SLACK_LOGS_CHANNEL")
 
+
 _client = s3_client()  # NOTE (Sam): should we move this to utils/s3?
 
 
 def log_audio_to_slack(audio_path):
-
     _client.upload_file(audio_path, LOGGING_BUCKET, audio_path)
     url = f"https://{LOGGING_BUCKET}.s3.amazonaws.com/{audio_path}"
     requests.post(
