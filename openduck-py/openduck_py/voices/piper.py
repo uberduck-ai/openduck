345e2fadb7d040832d50aef76d441b8bae160b5f
hecko <hecko@uberduck.ai>
2024-03-14 22:55:54 +0000

    black

--- openduck-py/openduck_py/voices/piper.py
+++ openduck-py/openduck_py/voices/piper.py
@@ -6,16 +6,21 @@ from piper.voice import PiperVoice
 
 from .settings import DEVICE
 
-model = PiperVoice.load("models/cartoon-boy-upbeat-piper.onnx",
-                        config_path="models/cartoon-boy-upbeat-piper.onnx.json",
-                        use_cuda=(DEVICE == "cuda"))
+model = PiperVoice.load(
+    "models/cartoon-boy-upbeat-piper.onnx",
+    config_path="models/cartoon-boy-upbeat-piper.onnx.json",
+    use_cuda=(DEVICE == "cuda"),
+)
 
-def inference(text: str, output_sample_rate: int, language: str = "english") -> np.ndarray:
+
+def inference(
+    text: str, output_sample_rate: int, language: str = "english"
+) -> np.ndarray:
     audio = model.synthesize_stream_raw(
         text,
-        speaker_id = 0,
+        speaker_id=0,
     )
     audio = b"".join(audio)
-    audio = torch.frombuffer(audio, dtype=torch.int16).float() / 32767 # TODO silly
+    audio = torch.frombuffer(audio, dtype=torch.int16).float() / 32767  # TODO silly
     audio = resample(audio, model.config.sample_rate, output_sample_rate)
     return audio
