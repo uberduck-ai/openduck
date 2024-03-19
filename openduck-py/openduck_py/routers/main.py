dfccc387f14c58eac5732807c43689a10da67d25
zach ocean <z@uberduck.ai>
2024-03-19 09:19:31 -0700

    fix cors

--- openduck-py/openduck_py/routers/main.py
+++ openduck-py/openduck_py/routers/main.py
@@ -18,7 +18,11 @@ app = FastAPI(
 # Add CORS middleware to allow requests from localhost:3000
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://openduck.vercel.app",
+        "https://openduck.ai",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
