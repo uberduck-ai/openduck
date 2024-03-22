2e716752f18c49870ee02bd49cea313e2fa00100
Matthew Kennedy <mtthwknnd4@gmail.com>
2024-03-22 19:03:31 +0000

    deepgram transcription

--- openduck-py/openduck_py/settings.py
+++ openduck-py/openduck_py/settings.py
@@ -1,4 +1,5 @@
 import os
+from typing import Literal
 
 HF_AUTH_TOKEN = os.environ.get("HF_AUTH_TOKEN")
 EMB_MATCH_THRESHOLD = 0.5
@@ -13,6 +14,10 @@ CHAT_MODEL = "azure/gpt-35-turbo-deployment"
 CHAT_MODEL_GPT4 = "azure/gpt-4-deployment"
 AUDIO_UPLOAD_BUCKET = os.environ.get("AUDIO_UPLOAD_BUCKET", "openduck-us-west-2")
 LOG_TO_S3 = True
+
+ASRMethod = Literal["deepgram", "whisper"]
+ASR_METHOD: ASRMethod = "deepgram"
+DEEPGRAM_API_SECRET = os.environ.get("DEEPGRAM_API_SECRET")
 
 # to not break existing env files
 os.environ["AZURE_API_KEY"] = os.environ.get("AZURE_OPENAI_API_KEY", "")
