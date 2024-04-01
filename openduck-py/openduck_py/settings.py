8a43a2a2b3612eb9aca13c3aff97815ba126e132
Matthew Kennedy <mtthwknnd4@gmail.com>
2024-04-01 12:03:40 -0700

    speech prob and settings refactor

--- openduck-py/openduck_py/settings.py
+++ openduck-py/openduck_py/settings.py
@@ -1,27 +1,34 @@
 import os
 from typing import Literal
 
-HF_AUTH_TOKEN = os.environ.get("HF_AUTH_TOKEN")
-EMB_MATCH_THRESHOLD = 0.5
-WS_SAMPLE_RATE = 16_000
-OUTPUT_SAMPLE_RATE = 24_000
+# Env settings and secrets
 IS_DEV = bool(os.environ.get("IS_DEV"))
 ML_API_URL = os.environ["ML_API_URL"]
+LOG_TO_SLACK = bool(os.environ.get("LOG_TO_SLACK", False))
+AUDIO_UPLOAD_BUCKET = os.environ.get("AUDIO_UPLOAD_BUCKET", "openduck-us-west-2")
+HF_AUTH_TOKEN = os.environ.get("HF_AUTH_TOKEN")
+DEEPGRAM_API_SECRET = os.environ.get("DEEPGRAM_API_SECRET")
+
+# Audio settings
+WS_SAMPLE_RATE = 16_000
+OUTPUT_SAMPLE_RATE = 24_000
 # Set to 1024 for the esp32, but larger CHUNK_SIZE is needed to prevent choppiness with the local client
 CHUNK_SIZE = 10240
-LOG_TO_SLACK = bool(os.environ.get("LOG_TO_SLACK", False))
-TEMPERATURE = 1.2
 
+EMB_MATCH_THRESHOLD = 0.5
+LOG_TO_S3 = False
+NO_SPEECH_PROB_THRESHOLD = 0.8
+
+# LLM settings
 ChatModels = Literal[
     "azure/gpt-35-turbo-deployment", "azure/gpt-4-deployment", "groq/mixtral-8x7b-32768"
 ]
 CHAT_MODEL = "azure/gpt-35-turbo-deployment"
-AUDIO_UPLOAD_BUCKET = os.environ.get("AUDIO_UPLOAD_BUCKET", "openduck-us-west-2")
-LOG_TO_S3 = False
+TEMPERATURE = 1.2
 
+# ASR settings
 ASRMethod = Literal["deepgram", "whisper"]
 ASR_METHOD: ASRMethod = "whisper"
-DEEPGRAM_API_SECRET = os.environ.get("DEEPGRAM_API_SECRET")
 
 # to not break existing env files
 os.environ["AZURE_API_KEY"] = os.environ.get("AZURE_OPENAI_API_KEY", "")
