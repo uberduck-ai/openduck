0df17ee4c18abfc7eba3b23436f09ba37b51a57d
williamluer <will.luer@gmail.com>
2024-04-03 21:44:03 +0000

    cleanup

--- openduck-py/openduck_py/routers/voice.py
+++ openduck-py/openduck_py/routers/voice.py
@@ -313,8 +313,6 @@ async def connect_daily(
         record=record,
         input_audio_format="int16",
         tts_config=tts_config,
-        # tts_config=TTSConfig(provider="elevenlabs", voice_id=voice_id),
-        # tts_config=TTSConfig(provider="azure"),
         system_prompt=system_prompt,
         context=base_context,
     )
